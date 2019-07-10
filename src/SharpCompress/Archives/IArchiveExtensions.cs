be0cab8ee1cf39e0b64efb6a81aa208b9b87764a
BipinBaglung <bhuwan.poudyal@nepasoft.com>
2019-07-10 12:06:55 +0545

    Add Extraction extension method to extract files from certain index only

--- src/SharpCompress/Archives/IArchiveExtensions.cs
+++ src/SharpCompress/Archives/IArchiveExtensions.cs
@@ -1,6 +1,6 @@
 ﻿#if !NO_FILE
-using System.Linq;
 using SharpCompress.Common;
+using System.Linq;
 
 #endif
 
@@ -10,13 +10,25 @@ namespace SharpCompress.Archives
     {
 #if !NO_FILE
 
-/// <summary>
-/// Extract to specific directory, retaining filename
-/// </summary>
+        /// <summary>
+        /// Extract to specific directory, retaining filename
+        /// </summary>
         public static void WriteToDirectory(this IArchive archive, string destinationDirectory,
                                             ExtractionOptions options = null)
         {
             foreach (IArchiveEntry entry in archive.Entries.Where(x => !x.IsDirectory))
+            {
+                entry.WriteToDirectory(destinationDirectory, options);
+            }
+        }
+
+        /// <summary>
+        /// Extract files at selected indexex to specefic directory 
+        /// </summary>
+        public static void WriteToDirectory(this IArchive archive, string destinationDirectory, int[] indexes,
+                                   ExtractionOptions options = null)
+        {
+            foreach (IArchiveEntry entry in archive.Entries.Where(x => !x.IsDirectory && indexes.Contains(x.Index)))
             {
                 entry.WriteToDirectory(destinationDirectory, options);
             }
